8e370da35d2dfbd9889faab62717e16d6957526a
Tommy Palmer <hi@tommyp.org>
2021-02-15 19:16:11 +0000

    add nav and header

---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Nav() {
+  return (
+    <nav>
+      <Link href="/products">Products</Link>
+      <Link href="/sell">Sell</Link>
+      <Link href="/orders">Orders</Link>
+      <Link href="/account">Account</Link>
+      <Link href="/cart">Cart</Link>
+    </nav>
+  );
+}
