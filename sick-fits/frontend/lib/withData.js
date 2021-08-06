f91749d4c4936ed32669ba63e4e2dd139e3e5cb9
Tommy Palmer <hi@tommyp.org>
2021-08-06 07:13:31 +0100

    add apollo client

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -9,16 +9,16 @@ function createClient({ headers, initialState }) {
   return new ApolloClient({
     link: ApolloLink.from([
       onError(({ graphQLErrors, networkError }) => {
-        if (graphQLErrors)
-          graphQLErrors.forEach(({ message, locations, path }) =>
-            console.log(
-              `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-            )
-          );
-        if (networkError)
+        if (graphQLErrors) {
+          graphQLErrors.forEach(({ message, locations, path }) => console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ));
+        }
+        if (networkError) {
           console.log(
-            `[Network error]: ${networkError}. Backend is unreachable. Is it running?`
+            `[Network error]: ${networkError}. Backend is unreachable. Is it running?`,
           );
+        }
       }),
       // this uses apollo-link-http under the hood, so all the options here come from that package
       createUploadLink({
