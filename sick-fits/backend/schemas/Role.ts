f402ce9dab0dd3b9c6a562355c7773f1ab444cd5
Tommy Palmer <hi@tommyp.org>
2022-02-15 17:13:59 +0000

    hide roles if not able to edit

--- sick-fits/backend/schemas/Role.ts
+++ sick-fits/backend/schemas/Role.ts
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { text, relationship } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
+import { permissions } from '../access';
 import { permissionFields } from './fields';
 
 export const Role = list({
+  access: permissions.canManageRoles,
+  ui: {
+    hideCreate: (args) => !permissions.canManageRoles(args),
+    hideDelete: (args) => !permissions.canManageRoles(args),
+    isHidden: (args) => !permissions.canManageRoles(args),
+  },
   fields: {
     name: text({ isRequired: true }),
     ...permissionFields,
