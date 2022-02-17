18673ace434d63b908656f80188826f69c4c5932
Tommy Palmer <hi@tommyp.org>
2022-02-17 15:58:56 +0000

    add sample test

---
+++ sick-fits/frontend/__tests__/sample.test.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+describe('Sample test 101', () => {
+  it('works as expected', () => {
+    // we run our expect statements to see if the test will pass
+    expect(1).toEqual(1);
+  });
+});
